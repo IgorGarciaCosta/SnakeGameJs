931f274118be36555938935ae2879bd636716c38
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-20 17:34:28 -0300

    add cobra e função pra começar jogo

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-let canvas =  document.getElementById("snake");
+let canvas = document.getElementById("snake");
 let context = canvas.getContext('2d');
 let box = 32;
-
-var criarBG=()=>{
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16*box, 16*box);
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBG();
+let direction = "right";
+
+var criarBG = () => {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+var criarCobra = () => {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+var inciarJogo = () => {
+    criarBG();
+    criarCobra();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if(direction=="up") snakeY -=box;
+    if(direction=="down") snakeY +=box;
+
+    snake.pop();
+
+    let newHead = {
+        x:snakeX,
+        y:snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(inciarJogo, 100);//dá continuidade ao jogo
+
