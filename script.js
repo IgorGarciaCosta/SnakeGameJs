c016b4c34c5233241e954313384db70919db444f
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-21 16:14:48 -0300

    condição de morte, pegar item, adição de css

--- script.js
+++ script.js
@@ -8,37 +8,55 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
-var criarBG = () => {
+function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-var criarCobra = () => {
+function criarCobra() {
     for (let i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);//chama a update
 
-function update(event){
+function update(event) {
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
     if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-var inciarJogo = () => {
+function inciarJogo() {
 
-    if(snake[0].x >15*box&&direction=="right") snake[0].x = 0;
-    if(snake[0].x <0*box&&direction=="left") snake[0].x = 15*box;
-    if(snake[0].y >15*box&&direction=="down") snake[0].y = 0;
-    if(snake[0].y <0*box&&direction=="up") snake[0].y = 15*box;
     
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 15 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 15 * box;
+
+    for(let i=1; i<snake.length;i++){//checar se cada coordenada do corpo chocou com a cabeça
+        if(snake[0].x==snake[i].x&&snake[0].y==snake[i].y){//chocou
+            clearInterval(jogo);
+            alert("FIM DE JOGO")
+        }
+    }
+
     criarBG();
     criarCobra();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -48,7 +66,15 @@ var inciarJogo = () => {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    //se não pegou o item
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();//tira o ultimo aumento de tamanho
+    }
+
+    else {//se pedou o item, aumenta
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
